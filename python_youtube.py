c84f270c0c10f856195bc179bb0854b4379dfe92
Gamez <uid32117@vitesco.com>
2022-02-13 00:42:15 -0600

    my first commit

---
+++ python_youtube.py
@@ -0,0 +1,15 @@
+import os
+from pytube import YouTube
+
+CURRENT_DIR = os.getcwd()
+DOWNLOAD_FOLDER = CURRENT_DIR
+video_url = "https://www.youtube.com/watch?v=sPNI8fpWklg"
+video_obj = YouTube(video_url)
+
+#stream = video_obj.streams.get_audio_only()
+stream = video_obj.streams.get_highest_resolution()
+#stream = video_obj.streams.get_lowest_resolution()
+stream.download(DOWNLOAD_FOLDER)
+
+print('='*80)
+print (DOWNLOAD_FOLDER)
